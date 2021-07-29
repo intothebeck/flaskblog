43ff26d3e46e53ccdae330e4f35bbb4e5c563449
intothebeck <dimanch71@mail.ru>
2021-07-29 22:38:58 +0300

    blog
    blog

---
+++ flaskblog/myblog/forms.py
@@ -0,0 +1,63 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from myblog.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    confirm_password = PasswordField('Подтвердите пароль',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Это имя пользователя занято.Попробуйте другое')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Этот email занят.Попробуйте другой.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember = BooleanField('Запомнить меня')
+    submit = SubmitField('Вход')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Имя пользователя',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Изменить фотографию', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Изменить')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Это имя занято. Пожалуйста, выберите другое.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Этот email занят. Пожалуйста, выберите другой.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Название', validators=[DataRequired()])
+    content = TextAreaField('Статья', validators=[DataRequired()])
+    submit = SubmitField('Выложить')
+
